0586ad48a6ed2521af59c483976a37b122885857
Ethan Budd <budde25@protonmail.com>
2021-05-08 22:16:24 -0500

    reformat

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-#![feature(panic_info_message,asm, global_asm, lang_items)]
+#![feature(panic_info_message, asm, global_asm, lang_items)]
 
 global_asm!(include_str!("arch/x86_64/boot_1.s"));
 global_asm!(include_str!("arch/x86_64/boot_2.s"));
@@ -10,33 +10,30 @@ mod vga;
 
 #[no_mangle]
 pub extern "C" fn kmain() {
-	// Main should initialize all sub-systems and get
-	// ready to start scheduling. The last thing this
-	// should do is start the timer.
+    // Main should initialize all sub-systems and get
+    // ready to start scheduling. The last thing this
+    // should do is start the timer.
 
-	// loop {}
+    // loop {}
 
     let hello = b"Hello World!";
     let color_byte = 0x1f; // white foreground, blue background
 
     let mut hello_colored = [color_byte; 24];
     for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
+        hello_colored[i * 2] = *char_byte;
     }
 
     // write `Hello World!` to the center of the VGA text buffer
     let buffer_ptr = (0xb8000 + 1988) as *mut _;
     unsafe { *buffer_ptr = hello_colored };
 
-    loop{}
+    loop {}
 }
 
 #[macro_export]
-macro_rules! print
-{
-	($($args:tt)+) => ({
-
-	});
+macro_rules! print {
+    ($($args:tt)+) => {{}};
 }
 #[macro_export]
 macro_rules! println
@@ -55,29 +52,26 @@ macro_rules! println
 #[lang = "eh_personality"]
 #[no_mangle]
 extern "C" fn eh_personality() {
-	loop { }
+    loop {}
 }
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-	print!("Aborting: ");
-	if let Some(p) = info.location() {
-		println!(
-					"line {}, file {}: {}",
-					p.line(),
-					p.file(),
-					info.message().unwrap()
-		);
-	}
-	else {
-		println!("no information available.");
-	}
-	abort();
+    print!("Aborting: ");
+    if let Some(p) = info.location() {
+        println!(
+            "line {}, file {}: {}",
+            p.line(),
+            p.file(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("no information available.");
+    }
+    abort();
 }
 
 #[no_mangle]
-extern "C"
-fn abort() -> ! {
-	loop {
-	}
-}		
+extern "C" fn abort() -> ! {
+    loop {}
+}
