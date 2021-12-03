c603839731d793970caf32a2e707855e28921638
Ethan Budd <budde25@protonmail.com>
2021-12-03 11:41:03 -0600

    addr math

--- src/address/phys.rs
+++ src/address/phys.rs
@@ -1,7 +1,9 @@
 use bit_field::BitField;
 use core::convert::TryFrom;
 use core::fmt::{self, Debug, Formatter};
+use core::ops::{Add, AddAssign, Sub, SubAssign};
 
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PhysicalAddress(u64);
 
 #[derive(Debug)]
@@ -30,6 +32,109 @@ impl TryFrom<u64> for PhysicalAddress {
     type Error = PhysicalAddressInvalid;
     fn try_from(value: u64) -> Result<Self, Self::Error> {
         Self::try_new(value)
+    }
+}
+
+impl TryFrom<usize> for PhysicalAddress {
+    type Error = PhysicalAddressInvalid;
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        Self::try_new(value as u64)
+    }
+}
+
+impl From<u32> for PhysicalAddress {
+    fn from(value: u32) -> Self {
+        Self::new(value as u64)
+    }
+}
+
+impl From<u16> for PhysicalAddress {
+    fn from(value: u16) -> Self {
+        Self::new(value as u64)
+    }
+}
+
+impl From<u8> for PhysicalAddress {
+    fn from(value: u8) -> Self {
+        Self::new(value as u64)
+    }
+}
+
+impl Add<PhysicalAddress> for PhysicalAddress {
+    type Output = Self;
+    fn add(self, rhs: PhysicalAddress) -> Self::Output {
+        PhysicalAddress::new(self.0 + rhs.0)
+    }
+}
+
+impl Add<u64> for PhysicalAddress {
+    type Output = Self;
+    fn add(self, rhs: u64) -> Self::Output {
+        PhysicalAddress::new(self.0 + rhs)
+    }
+}
+
+impl Add<usize> for PhysicalAddress {
+    type Output = Self;
+    fn add(self, rhs: usize) -> Self::Output {
+        PhysicalAddress::new(self.0 + rhs as u64)
+    }
+}
+
+impl AddAssign<PhysicalAddress> for PhysicalAddress {
+    fn add_assign(&mut self, rhs: PhysicalAddress) {
+        *self = *self + rhs.0;
+    }
+}
+
+impl AddAssign<usize> for PhysicalAddress {
+    fn add_assign(&mut self, rhs: usize) {
+        *self = *self + rhs as u64;
+    }
+}
+
+impl AddAssign<u64> for PhysicalAddress {
+    fn add_assign(&mut self, rhs: u64) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub<PhysicalAddress> for PhysicalAddress {
+    type Output = Self;
+    fn sub(self, rhs: PhysicalAddress) -> Self::Output {
+        PhysicalAddress::new(self.0 - rhs.0)
+    }
+}
+
+impl Sub<u64> for PhysicalAddress {
+    type Output = Self;
+    fn sub(self, rhs: u64) -> Self::Output {
+        PhysicalAddress::new(self.0 - rhs)
+    }
+}
+
+impl Sub<usize> for PhysicalAddress {
+    type Output = Self;
+    fn sub(self, rhs: usize) -> Self::Output {
+        PhysicalAddress::new(self.0 - rhs as u64)
+    }
+}
+
+impl SubAssign<PhysicalAddress> for PhysicalAddress {
+    fn sub_assign(&mut self, rhs: PhysicalAddress) {
+        *self = *self - rhs.0;
+    }
+}
+
+impl SubAssign<usize> for PhysicalAddress {
+    fn sub_assign(&mut self, rhs: usize) {
+        *self = *self - rhs as u64;
+    }
+}
+
+impl SubAssign<u64> for PhysicalAddress {
+    fn sub_assign(&mut self, rhs: u64) {
+        *self = *self - rhs;
     }
 }
 
