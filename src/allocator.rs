f4dab9eee18fdc9b3291b49e3a7249c81841fba5
Ethan Budd <budde25@protonmail.com>
2021-12-04 23:02:21 -0600

    allocator heap

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,6 @@
 use block_alloc::Allocator;
 
-pub const HEAP_START: u64 = 0x_4444_4444_0000;
+pub const HEAP_START: u64 = 0x1000000;
 pub const HEAP_SIZE: u64 = 100 * 1024; // 100 KiB
 
 #[global_allocator]
