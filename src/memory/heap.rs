eaa801e400603d02d78fa5c0667a3b8d287d16f7
Ethan Budd <budde25@protonmail.com>
2022-01-18 15:23:27 -0600

    allocation is now a critical section

--- src/memory/heap.rs
+++ src/memory/heap.rs
@@ -1,14 +1,48 @@
+use core::alloc::GlobalAlloc;
+
 use crate::consts::{HEAP_SIZE, HEAP_START};
-use block_alloc::Allocator;
+use block_alloc::Allocator as BlockAllocator;
 
 #[global_allocator]
 static ALLOCATOR: Allocator = Allocator::new();
 
 pub fn init() {
-    unsafe { ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE) };
+    unsafe { ALLOCATOR.init(HEAP_START, HEAP_SIZE) };
     crate::kprintln!(
         "Kernel heap initialized at {:#X}, of size {:#X}",
         HEAP_START,
         HEAP_SIZE
     )
+}
+
+struct Allocator {
+    allocator: BlockAllocator,
+}
+
+impl Allocator {
+    pub const fn new() -> Self {
+        Self {
+            allocator: BlockAllocator::new(),
+        }
+    }
+
+    pub unsafe fn init(&self, heap_start: u64, heap_size: u64) {
+        self.allocator.lock().init(heap_start, heap_size)
+    }
+}
+
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        // critical section
+        crate::interrupts::disable_interrupts();
+        let ptr = self.allocator.alloc(layout);
+        crate::interrupts::enable_interrupts();
+        ptr
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        crate::interrupts::disable_interrupts();
+        self.allocator.dealloc(ptr, layout);
+        crate::interrupts::enable_interrupts();
+    }
 }
